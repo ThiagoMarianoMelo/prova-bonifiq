21dc99db6e8c2040b196bdb2e2405117a0e1dab1
Thiago Mariano de Melo <thiago.17mel@gmail.com>
2025-08-16 11:03:54 -0300

    fix: Criação de uma classe DTO com mapeamento para utilizar conversão de data para LocalTimeZone

---
+++ Models/Response/OrderCreatedResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace ProvaPub.Models.Response
+{
+    public class OrderCreatedResponse
+    {
+        public OrderCreatedResponse(Order order)
+        {
+            Id = order.Id;
+            Value = order.Value;
+            CustomerId = order.CustomerId;
+            OrderDate = order.OrderDate.ToLocalTime();
+        }
+
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
