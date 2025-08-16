ce4cc37b196b0eabb3ab2915276eee1bc2a34bd2
Thiago Mariano de Melo <thiago.17mel@gmail.com>
2025-08-15 23:18:51 -0300

    doc: Adição de comentários sobre solução Parte1

--- Controllers/Parte1Controller.cs
+++ Controllers/Parte1Controller.cs
@@ -25,3 +25,18 @@ namespace ProvaPub.Controllers
 		}
 	}
 }
+
+
+//1 - Problema do número repetido
+//O problema ocorria porque o serviço estava configurado como singleton, o que fazia com que a instância fosse criada apenas uma vez durante o ciclo de vida da aplicação. Como a seed do gerador aleatório era criada no construtor da classe, havia apenas uma seed fixa durante toda a execução.
+
+//Assim, o número gerado era sempre o mesmo. A solução foi alterar o tempo de vida do serviço para scoped, garantindo uma nova instância a cada requisição.
+
+//Importante ressaltar que essa não é a única solução. Poderíamos, por exemplo, gerar a seed dentro do próprio método, em tempo de request. No entanto, optei por usar o serviço com tempo de vida por escopo, pois isso permite aproveitar a injeção de dependência do DbContext.
+
+//2 - Erro de inserção duplicada
+//O segundo problema refere-se à exceção lançada ao tentar salvar números duplicados no banco de dados. Isso ocorre porque há um índice único definido no campo "Number" nas migrations, impedindo duplicações.
+
+//Uma solução mais simples seria remover o índice único, permitindo valores repetidos.
+
+//Contudo, se a intenção for manter a unicidade, é necessário tratar a exceção ao tentar inserir um valor já existente. Nesse caso, podemos capturar a exceção específica e retornar um erro 400 (Bad Request) informando ao cliente que o número já foi gerado anteriormente.
